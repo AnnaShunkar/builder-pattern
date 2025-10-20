51b2571c28f8abd739ab6056f22a1f885cf13122
AnnaShunkar <anna.shunkar4@gmail.com>
2025-10-20 13:03:25 +0300

    Add files via upload

---
+++ builder-pattern.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+
+class Message {
+private:
+    string header;
+    string body;
+    string footer;
+public:
+    void setHeader(const string& h) { header = h; }
+    void setBody(const string& b) { body = b; }
+    void setFooter(const string& f) { footer = f; }
+
+    void show() const {
+        cout << header << endl;
+        cout << body << endl;
+        cout << footer << endl;
+    }
+};
+
+// будівельник
+class MessageBuilder {
+private:
+    Message* msg;
+public:
+    MessageBuilder() { msg = new Message(); }
+    MessageBuilder& addHeader(const string& h) {
+        msg->setHeader(h);
+        return *this;
+    }
+
+    MessageBuilder& addBody(const string& b) {
+        msg->setBody(b);
+        return *this;
+    }
+
+    MessageBuilder& addFooter(const string& f) {
+        msg->setFooter(f);
+        return *this;
+    }
+
+    Message* build() {
+        return msg;
+    }
+};
+
+int main() {
+    MessageBuilder builder;
+    Message* message = builder
+        .addHeader("=== Message ===")
+        .addBody("builder pattern")
+        .addFooter("=== Finish ===")
+        .build();
+
+    message->show();
+    return 0;
+}
